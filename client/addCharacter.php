7c4295abb4969fc3295b96fec0bd380305b7e7df
meynm <meynmd@users.noreply.gibhub.com>
2015-11-13 21:04:43 -0800

    character pages added

---
+++ client/addCharacter.php
@@ -0,0 +1,51 @@
+<?php
+	// turn error reporting on
+	ini_set('display_errors', 'On');
+	$cFile = fopen("credentials.txt", "r");
+	$cInfo = fscanf($cFile, "%s\t%s\t%s\t%s");
+	// create a new mysqli object that connects to the database
+	$mysqli = new mysqli($cInfo[0], $cInfo[1], $cInfo[2], $cInfo[3]);
+	// if there's an error, report it:
+	if($mysqli->connect_errno) {
+		echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+
+// get the info from the post
+$creator = $_POST['creator'];
+$class = $_POST['classRef'];
+$name = $_POST['name'];
+$level = $_POST['level'];
+$health = $_POST['health'];
+$strength = $_POST['strength'];
+
+echo "creator = " . $creator . "\nclassRef = ". $class;
+
+// prepare the query and bind the variables
+$stmt = $mysqli->prepare("
+	INSERT INTO playerCharacter (characterName, level, health, strength, playerId, classId) 
+	VALUES (?, ?, ?, ?, ?, ?);");
+$stmt->bind_param('siiiii', $name, $level, $health, $strength, $creator, $class);
+
+// execute and report the result
+$status = $stmt->execute();
+if($status) {
+	echo "Added " . $stmt->affected_rows . " rows to playerCharacter.";
+} else {
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+}
+	
+$stmt->close();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8"/>
+	<title>Game Database</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<form method="GET" action="playerCharacters.php">
+		<input type="submit" value="Return to Character Table"/>
+	</form>
+</body>
---
+++ client/playerCharacters.php
@@ -0,0 +1,142 @@
+<?php
+	// turn error reporting on
+	ini_set('display_errors', 'On');
+	$cFile = fopen("credentials.txt", "r");
+	$cInfo = fscanf($cFile, "%s\t%s\t%s\t%s");
+	// create a new mysqli object that connects to the database
+	$mysqli = new mysqli($cInfo[0], $cInfo[1], $cInfo[2], $cInfo[3]);
+	// if there's an error, report it:
+	if($mysqli->connect_errno) {
+		echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8"/>
+	<title>Game Database</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<!-- table of characters -->
+	<div id="playersTable">
+		<table>
+			<caption>Characters</caption>
+			<tr>
+
+				<th>Character ID</th>
+				<th>Name</th>
+				<th>Belongs to Player</th>				
+				<th>Class</th>
+				<th>Level</th>
+				<th>Health</th>
+				<th>Strength</th>
+			</tr>
+			
+<!-- Now, populate the table -->
+<?php
+	// prepare statement to get the contents of 'player'
+	$stmt = $mysqli->prepare("
+		SELECT pc.id, characterName, p.userName, cc.className, level, health, strength
+		FROM playerCharacter pc INNER JOIN player p ON pc.playerId = p.id
+		INNER JOIN characterClass cc ON pc.classId = cc.id
+		ORDER BY p.userName;
+	");
+	
+	if(!$stmt) {
+		echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+	}
+	// execute the statement
+	if(!$stmt->execute()) {
+		echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	// bind the results to variables and display the results
+	$stmt->bind_result($cId, $cName, $cOwner, $cClass, $cLevel, $cHealth, $cStrength);	
+	if(!($stmt)) {
+		echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	while($stmt->fetch()){
+		echo "<tr>\n<td>\n" . 
+			$cId . "</td>\n<td>" . 
+			$cName . "</td>\n<td>" . 
+			$cOwner . "</td>\n<td>" . 
+			$cClass . "</td>\n<td>" . 
+			$cLevel . "</td>\n<td>" . 
+			$cHealth . "</td>\n<td>" . 
+			$cStrength . "\n</td>\n</tr>";
+	}
+	$stmt->close();
+?>
+		</table>
+	</div>
+	
+<!-- form to add new characters -->	
+	<div id="addCharacterForm">
+		<form method="post" action="addCharacter.php">
+			<fieldset>
+				<legend>Add New Character</legend>
+				<p>
+					<select name="classRef">
+
+<?php
+	// get a list of classes
+	$stmt = $mysqli->prepare("SELECT id FROM characterClass ORDER BY id;");
+	if(!$stmt) {
+		echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+	}
+	// execute the statement
+	if(!$stmt->execute()){
+		echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	// bind the results to variables and display the results
+	if(!$stmt->bind_result($classId)){
+		echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	while($stmt->fetch()){
+	 echo "<option value=\"" . $classId . "\">" . $classId . ": " . "</option>";
+	}
+	$stmt->close();
+?>
+
+					</select>
+				</p>
+				
+				<p>
+					<select name="creator">
+
+<?php
+	// get a list of players
+	$stmt = $mysqli->prepare("SELECT id, userName FROM player ORDER BY id;");
+	if(!$stmt) {
+		echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+	}
+	// execute the statement
+	if(!$stmt->execute()){
+		echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	// bind the results to variables and display the results
+	if(!$stmt->bind_result($playerId, $playerName)){
+		echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	while($stmt->fetch()){
+	 echo "<option value=\"" . $playerId . "\">" . $playerId . ": " . $playerName . "</option>";
+	}
+	$stmt->close();
+?>
+					</select>
+				</p>
+
+				<p>Name: <input type="text" name="name" required/></p>
+				<p>Level: <input type="text" name="level" required/></p>
+				<p>Health: <input type="text" name="health" required/></p>		
+				<p>Strength: <input type="text" name="strength" required></p>												
+				<p><input type="submit" name="addNew" value="Add Character" /></p>
+				
+			</fieldset>
+		</form>
+	</div>
+</body>
+</html>
