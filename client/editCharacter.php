64b633e3f0d768af99021ac85b19008826d3f61b
Matthew Meyn <meynm@flip3.engr.oregonstate.edu>
2015-12-04 20:10:01 -0800

    made it possible to edit character attributes

---
+++ client/editCharacter.php
@@ -0,0 +1,44 @@
+<?php require("config.php");
+checkSession();
+
+$charId = $_POST['id'];
+$level = $_POST['level'];
+$health = $_POST['health'];
+$strength = $_POST['strength'];
+
+// prepare the query and bind the variables
+$stmt = $mysqli->prepare("
+		UPDATE playerCharacter SET 
+			level = ?, 
+			health = ?, 
+			strength = ?
+			WHERE id = ?;
+");
+
+$stmt->bind_param('iiii', $level, $health, $strength, $charId);
+
+// execute and report the result
+$status = $stmt->execute();
+if($status) {
+	echo "Changed " . $stmt->affected_rows . " rows in playerCharacter.";
+} else {
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+}
+	
+$stmt->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8"/>
+	<title>Game Database</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<form method="GET" action="playerCharacters.php">
+		<input type="submit" value="Return to Characters Table"/>
+	</form>
+
+</body>
